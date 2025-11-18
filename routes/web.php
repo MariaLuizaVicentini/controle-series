3ce58532c4e504d5dc90ebb60a9ae2df1506f919
MaluVicentinio <vicentinimalu1@gmail.com>
2025-11-18 16:18:32 -0300

    remove: function welcome do laravel

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,4 @@
 use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Route::get('/series', [SeriesController::class, 'listarSeries']);
