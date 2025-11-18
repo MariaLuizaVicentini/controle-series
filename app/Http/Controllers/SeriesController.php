e6fcab923f3dece3366e1715b7dad68a8ac24431
MaluVicentinio <vicentinimalu1@gmail.com>
2025-11-18 17:02:35 -0300

    feat: adiciona um novo arquivo de view para exibir as series listadas

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -1,23 +1,15 @@
 <?php 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
 
 class SeriesController extends Controller {
-    public function index(Request $request) {
+    public function index() {
         $series = [
             'Grey\'s Anatomy',
             'Lost',
             'Agents of SHIELD'
         ];
 
-        $html = "<ul>";
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-
-        $html .= "</ul>";
-
-        return $html;
+        return view('series.index', compact('series'));
     }
 }
 
