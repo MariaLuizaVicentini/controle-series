26b37be08ecb375714e20a22c07bcebf2bb8b589
Malu Vicentini <vicentinimalu1@gmail.com>
2025-11-20 22:22:18 -0300

    refactor: usando comando blade na view index

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -8,8 +8,8 @@
     <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
 
     <ul class="list-group">
-        <?php foreach ($series as $serie): ?>
+        @foreach($series as $serie)    
         <li class="list-group-item"><?=$serie?></li>
-        <?php endforeach;?>
+        @endforeach
     </ul>
 @endsection
