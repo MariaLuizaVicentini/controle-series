720dcdc9a1259da1e5d0f265df29cd0df0d2cadf
Malu Vicentini <vicentinimalu1@gmail.com>
2025-12-19 00:28:59 -0300

    fix: series creation service

--- app/Services/CriadorDeSerie.php
+++ app/Services/CriadorDeSerie.php
@@ -5,23 +5,34 @@ namespace App\Services;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CriadorDeSerie 
 {
-    public function criarSeries(
-        string $nomeSerie,
-        int $qtdTemporadas,
-        int $epPorTemporada
-    ): Serie {
+    public function criarSeries(string $nomeSerie, int $qtdTemporadas, int $epPorTemporada) : Serie 
+    {
+        DB::beginTransaction();
         $serie = Serie::Create(['nome' => $nomeSerie]);
-        for($i = 1; $i <= $qtdTemporadas; $i++) {
+        $this->criaTemporadas($qtdTemporadas, $epPorTemporada, $serie);
+        DB::commit();
+
+        return $serie;
+    }
+
+    public function criaTemporadas(int $qtdTemporadas, int $epPorTemporada, $serie) 
+    {
+        for ($i=1; $i <= $qtdTemporadas; $i++) {
             $temporada = $serie->temporadas()->create(['numero' => $i]);
 
-            for ($j = 1; $j <= $epPorTemporada; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
+            $this->criaEpisodios($epPorTemporada, $temporada);
         }
-        return $serie;
+    }
+
+    public function criaEpisodios( int $epPorTemporada, \Illuminate\Database\Eloquent\Model $temporada ) : void 
+    {
+        for ($j =1; $j <= $epPorTemporada; $j++) {
+            $temporada->episodios()->create(['numero' => $j]);
+        }
     }
 }
 
