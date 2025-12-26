46fd4b5e916ad775ba3ed503aa07e4e5a1ea8fc1
Malu Vicentini <vicentinimalu1@gmail.com>
2025-12-25 23:19:52 -0300

    feat: adds a route that accesses the method to watch episodes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::post('/series/{id}/editaNome', [SeriesController::class, 'editaNome']);
 Route::get('/series/{serieId}/temporadas', [TemporadasController::class, 'index']);
 
 Route::get('/temporadas/{temporada}/episodios', [EpisodiosController::class, 'index']);
+
+Route::post('/temporadas/{temporada}/episodios/assistir', [EpisodiosController::class, 'assistir']);
