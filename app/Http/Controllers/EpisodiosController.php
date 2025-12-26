a431ebcce8f6952db11d37a3d95851a5379b62e2
Malu Vicentini <vicentinimalu1@gmail.com>
2025-12-25 23:15:25 -0300

    feat: mark episodes as watched by season

--- app/Http/Controllers/EpisodiosController.php
+++ app/Http/Controllers/EpisodiosController.php
@@ -13,7 +13,22 @@ class EpisodiosController extends Controller
 {
     public function index(Temporada $temporada)
     {
-        $episodios = $temporada->episodios;
-        return view('episodios.index', compact('episodios'));
+        return view('episodios.index', [
+            'episodios' => $temporada->episodios,
+            'temporadaId' => $temporada->id
+        ]);
     }
+
+    public function assistir(Temporada $temporada, Request $request)
+    {
+        $episodiosAssistidos = $request->episodios;
+        $temporada->episodios->each(function (Episodio $episodio) use ($episodiosAssistidos) {
+            $episodio->assistido = in_array(
+                $episodio->id, 
+                $episodiosAssistidos
+            );
+        });
+        $temporada->push();
+    }
+
 }
